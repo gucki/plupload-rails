01beba84dab152e986b4e8d55e4f0452ac8e2129
Corin Langosch <info@corinlangosch.com>
2011-10-04 18:19:00 +0200

    first commit

---
+++ lib/plupload-rails.rb
@@ -0,0 +1,7 @@
+require "plupload-rails/version"
+
+module Plupload
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/plupload-rails/version.rb
@@ -0,0 +1,5 @@
+module Plupload
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
